23a28e7e01fdde80e471fde7af2ea42da0287ecf
Zach5812 <zachlouk7@gmail.com>
2023-05-04 10:37:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import SubContent from './components/SubContent';
+import Advertisement from './components/Advertisement';
+import Main from './components/Main';
 
 function App() {
   return (
     <div>
-      <h1>Hello Dojo!</h1>
-      <h3>Things I need to do:</h3>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt. Everest</li>
-        <li>Run a Marathon</li>
-        <li>Feed the Dogs</li>
-      </ul>
+      <Header className="header" />
+      <div className="bottom">
+        <Navigation className="navigation" />
+        <Main className="main" >
+        <div className="contents">
+          <SubContent className="subcontent" />
+          <SubContent className="subcontent" />
+          <SubContent className="subcontent" />
+        </div>
+        <Advertisement className="ad" />
+        </Main>
+      </div>
     </div>
   );
 }
